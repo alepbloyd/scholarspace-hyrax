dd5af97ae11edf60a7709301bc7aafae55d6006a
Alex Boyd <alexanderleeboyd@gmail.com>
2024-01-25 19:00:03 +0000

    test

--- docker/scripts/scholarspace-solr-init.sh
+++ docker/scripts/scholarspace-solr-init.sh
@@ -13,21 +13,16 @@ for solr_core in "$SOLR_CORE"_test "$SOLR_CORE"_dev $SOLR_CORE; do
         echo "----1----"
         echo "Creating ${solr_core} core"
         . /opt/docker-solr/scripts/run-initdb
+        echo "----ran initdb----"
+        
+        echo "----2----"
+        setuser solr /opt/docker-solr/scripts/precreate-core $solr_core
 
-        # echo "----2----"
-        # exec ls -alrth /opt/solr
-
-        echo "----3----"
-        # mkdir -p /opt/solr/server/solr/mycores/${solr_core}
-        # exec ls -alrth /opt/solr/server/solr/mycores
-
-        echo "----4----"
-        exec /opt/docker-solr/scripts/precreate-core $solr_core
         # ScholarSpace-specific setup
         echo "Disabling managed schema"
-        mv /opt/solr/server/solr/mycores/${solr_core}/conf/managed-schema /opt/solr/server/solr/mycores/${solr_core}/conf/managed-schema.bak
+        setuser solr mv /opt/solr/server/solr/mycores/${solr_core}/conf/managed-schema /opt/solr/server/solr/mycores/${solr_core}/conf/managed-schema.bak
         echo "Migrating configs"
-        cp -r /opt/scholarspace/solr/conf /opt/solr/server/solr/mycores/${solr_core}
+        setuser solr cp -r /opt/scholarspace/solr/conf /opt/solr/server/solr/mycores/${solr_core}
         echo "Starting Solr with new core..."
     else
         echo "Core ${solr_core} already exists!"
@@ -35,4 +30,4 @@ for solr_core in "$SOLR_CORE"_test "$SOLR_CORE"_dev $SOLR_CORE; do
 done
 
 echo "Starting Solr ..."
-exec /opt/docker-solr/scripts/solr-foreground
+setuser solr exec /opt/docker-solr/scripts/solr-foreground
