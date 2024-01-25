2ced418182cef991a905aa95840b26f60e452d8b
Alex Boyd <alexanderleeboyd@gmail.com>
2024-01-25 16:42:00 +0000

    testing solr

--- docker/scripts/scholarspace-solr-init.sh
+++ docker/scripts/scholarspace-solr-init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # something with the file paths in here not working with the github runner?
-# 
+# root vs. 
 
 set -e
 
@@ -10,20 +10,14 @@ for solr_core in "$SOLR_CORE"_test "$SOLR_CORE"_dev $SOLR_CORE; do
     if [ ! -d "/opt/solr/server/solr/mycores/${solr_core}" ]
     then
         # Replicated from the solr-create script
-        echo "Checkpoint -1"
-        echo "hello hellloooooo hhelllooooooooooo"
-        # echo "Creating ${solr_core} core"
-        echo "Checkpoint -0.5"
+        echo "Creating ${solr_core} core"
         . /opt/docker-solr/scripts/run-initdb
-        echo "Checkpoint 0"
-        /opt/docker-solr/scripts/precreate-core $solr_core # this one???
-        echo "Checkpoint 1"
+        /opt/docker-solr/scripts/precreate-core $solr_core
         # ScholarSpace-specific setup
         echo "Disabling managed schema"
         mv /opt/solr/server/solr/mycores/${solr_core}/conf/managed-schema /opt/solr/server/solr/mycores/${solr_core}/conf/managed-schema.bak
-        echo "Checkpoint 2"
         echo "Migrating configs"
-        cp -r /opt/scholarspace/solr/conf /opt/solr/server/solr/mycores/${solr_core} # this one???
+        cp -r /opt/scholarspace/solr/conf /opt/solr/server/solr/mycores/${solr_core}
         echo "Starting Solr with new core..."
     else
         echo "Core ${solr_core} already exists!"
