b31e2bcb640023df4291c7680f71d64aca18c9af
Alex Boyd <alexanderleeboyd@gmail.com>
2024-01-25 18:22:54 +0000

    test

--- docker/scripts/scholarspace-solr-init.sh
+++ docker/scripts/scholarspace-solr-init.sh
@@ -16,8 +16,10 @@ for solr_core in "$SOLR_CORE"_test "$SOLR_CORE"_dev $SOLR_CORE; do
         echo "----2----"
         . /opt/docker-solr/scripts/run-initdb
 
+        exec ls -alrth /opt
+
         echo "----3----"
-        sudo mkdir -p /opt/solr/server/solr/mycores/${solr_core}
+        mkdir -p /opt/solr/server/solr/mycores/${solr_core}
 
         echo "----4----"
         /opt/docker-solr/scripts/precreate-core $solr_core
